affcf20e6923a988b0f531aae77cb2a15168694c
David Cramer <david@digilab.co.za>
2015-02-07 13:07:03 +0200

    global config

--- index.php
+++ index.php
@@ -8,12 +8,6 @@ $_debug_start_time = $_debug_time;
 // SETUP up the params variable to be passed to the routed file.
 global $params;
 
-// Setup error reporting.
-set_error_handler( "errorHandler" );
-ini_set( 'display_errors', 1 );
-error_reporting( E_ALL );
-
-
 /// DEFINE ERROR HANDLE
 function errorHandler( $errno, $errstr, $errfile, $errline ) {
 	error_log( "Error: [$errno] $errstr" );
@@ -21,19 +15,47 @@ function errorHandler( $errno, $errstr, $errfile, $errline ) {
 	die;
 }
 
-
+// load config if exists
+$config = array();
+if( file_exists( dirname( __FILE__ ) . '/config.json' ) ){
+	$is_config = json_decode( file_get_contents( dirname( __FILE__ ) . '/config.json' ), true );
+	if( !empty( $is_config ) ){
+		$config = $is_config;
+		if( !empty( $config['debug'] ) ){
+			// Setup error reporting.
+			set_error_handler( "errorHandler" );
+			ini_set( 'display_errors', 1 );
+			error_reporting( E_ALL );
+		}
+	}
+}
 
 // GET URL
 $trueuri = ltrim( str_replace( str_replace( trim( $_SERVER['DOCUMENT_ROOT'], '/' ), '', trim( realpath( __dir__ ), '/' ) ), '', $_SERVER['REQUEST_URI'] ), '/' );
 $trueuri = preg_replace( "/[\/]{2,}/", '/', $trueuri );
 
 $urlstruct = parse_url( $trueuri );
-$pathVars = explode( '/', $urlstruct['path'] );
+if( isset( $urlstruct['path'] ) ){
+	$urlstruct['path'] = trim( $urlstruct['path'], '/' );
+}else{
+	$urlstruct['path'] = null;
+}
+
+$pathVars = explode( '/',  $urlstruct['path'] );
 
 // API VERSION
-define( 'VERSION', array_shift( $pathVars ) );
-
-// SOME CCONSTANTS
+if( !empty($pathVars[0]) && file_exists( dirname( __FILE__ ) . '/' . $pathVars[0] .'/' ) ){
+	// SOME CCONSTANTS - since the version is true.
+	define( 'VERSION', array_shift( $pathVars ) );
+}else{
+	// version not valid, use default
+	if( isset( $config['version'] ) ){
+		define( 'VERSION', $config['version'] );
+	}else{
+		define( 'VERSION', null );
+	}
+}
+// set constants
 define( 'ABSPATH', dirname( __FILE__ ) . '/' . VERSION .'/' );
 define( 'ABSURL', str_replace( '//', '/', dirname( $_SERVER['SCRIPT_NAME'] ).'/' ) );
 
@@ -49,7 +71,7 @@ $globalDebug = false;
 $resources = glob( ABSPATH . 'resources/*.json' );
 $routes = array();
 
-foreach ( $resources as &$resource ) {
+foreach ( $resources as &$resource ) {	
 	$fileData = json_decode( file_get_contents( $resource ), true );
 	if ( is_array( $fileData ) ) {
 		// is type?
@@ -78,18 +100,19 @@ foreach ( $resources as &$resource ) {
 		$routes = array_merge( $routes, $fileData );
 	}
 }
+
 foreach ( $routes as &$try_route ) {
 
-	if ( isset( $try_route['methods'] ) ) {
+	if ( isset( $try_route['methods'][$_SERVER['REQUEST_METHOD']] ) ) {
 		if ( isset( $try_route['version'] ) ) {
 			$try_route['name'] = $try_route['version'].'/'.$try_route['name'];
 		}
 		if ( !empty( $try_route['name'] ) ) {
-			$try_route['name'] = ltrim( $try_route['name'], '/' );
+			$try_route['name'] = trim( $try_route['name'], '/' );
 			$routeVars = explode( '/', $try_route['name'] );
-		}
-
+		}		
 		if ( count( $routeVars ) !== count( $pathVars ) ) {
+			
 			continue;
 		}
 		if ( $trueuri == $try_route['name'] ) {
@@ -98,7 +121,7 @@ foreach ( $routes as &$try_route ) {
 		}
 		if ( false !== strpos( $try_route['name'], ':' ) ) { //has vars, check if match
 			$testUrl = preg_replace( "/\\\:([a-zA-Z0-9_]+)/", '([a-zA-Z0-9_\-\%]+)', preg_quote( $try_route['name'], '/' ) );
-			preg_match_all( "/".$testUrl."/", $urlstruct['path'], $urlvars );
+			preg_match_all( "/".$testUrl."/", $trueuri, $urlvars );
 			if ( empty( $urlvars[1] ) ) {
 				continue; //no var match
 			}
@@ -184,22 +207,66 @@ if ( isset( $route ) ) {
 			// SEND ROUTE HEADERS
 			if ( !empty( $route['headers'] ) ) {
 				foreach ( $route['headers'] as $header=>&$value ) {
+					// find tags
+					if( substr( $value ,0, 1) === '%' && substr( $value ,strlen( $value )-1, 1) === '%'){
+						parse_str( str_replace('%', '', $value) , $vars);
+						foreach($vars as $type=>$str){
+							switch ($type) {
+								case 'date':
+									$value = str_replace("+0000", "GMT", gmdate('r', strtotime($str)));
+									break;
+								
+								case 'hash':
+									if ( empty( $_output ) || (int)$_output === 1 ) { // check that last header did not return;
+										if( $str === 'md5' ){
+											$value = md5( file_get_contents( ABSPATH . $route['methods'][$_SERVER['REQUEST_METHOD']]['file'] ) );
+										}elseif( $str === 'sha1' ){
+											$value = md5( file_get_contents( ABSPATH . $route['methods'][$_SERVER['REQUEST_METHOD']]['file'] ) );
+										}
+									}
+									break;
+								
+								default:
+									# code...
+									break;
+							}
+						}
+					}
 
-					/*if(false !== strpos($value, '[[') && false !== strpos($value, ']]')){
-						// header value can be a PHP function if wrapped in [[ phpfunction/code ]]
-						$_value = function(&$value){
-							// to prevent contaminated values;
-							$value = eval('return '.substr($value,2,strlen($value)-4).';');
-						};
-						$_value(&$value);
-					}*/
 					header( $header.': '.$value, true ); // true to overide any sent by globals
 				}
 			}
 			// SEND METHOD HEADERS
 			if ( !empty( $route['methods'][$_SERVER['REQUEST_METHOD']]['headers'] ) ) {
 				foreach ( $route['methods'][$_SERVER['REQUEST_METHOD']]['headers'] as $header=>&$value ) {
+					// find tags
+					if( substr( $value ,0, 1) === '%' && substr( $value ,strlen( $value )-1, 1) === '%'){
+						parse_str( str_replace('%', '', $value) , $vars);
+						foreach($vars as $type=>$str){
+							switch ($type) {
+								case 'date':
+									$value = str_replace("+0000", "GMT", gmdate('r', strtotime($str)));
+									break;
+
+								case 'hash':
+									if ( empty( $_output ) || (int)$_output === 1 ) { // check that last header did not return;
+										if( $value === 'md5' ){
+											$value = md5( file_get_contents( ABSPATH . $route['methods'][$_SERVER['REQUEST_METHOD']]['file'] ) );
+										}elseif( $value=='sha1' ){
+											$value = md5( file_get_contents( ABSPATH . $route['methods'][$_SERVER['REQUEST_METHOD']]['file'] ) );
+										}
+									}
+									break;
+								
+								default:
+									# code...
+									break;
+							}
+						}
+					}
+
 					header( $header.': '.$value, true ); // true to overide any sent by route
+					
 				}
 			}
 			if ( empty( $_output ) || (int)$_output === 1 ) { // check that last header did not return;
